faef073994b582275affaf9c93417a6452edf122
Marceldohbit <168606990+Marceldohbit@users.noreply.github.com>
2024-05-05 23:48:46 +0100

    Update Bupup.jsx

--- src/components/LoginComponent/Bupup.jsx
+++ src/components/LoginComponent/Bupup.jsx
@@ -4,7 +4,8 @@ const Bupup = () => {
   return (
    <div> 
     <div className='bupup'>
-      B U P U P
+      B U P U Prendergast is doing it again.. 
+I want to see if this will  work 
     </div>
     <div className="introduce">Sign Up</div>
     </div> 
